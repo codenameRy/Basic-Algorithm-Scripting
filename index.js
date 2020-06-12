ffa6eb9cd16c0a362685e5d7d798cae8ec2bc188
Ryan Lee <ryanm.lee11@gmail.com>
2020-06-12 03:33:36 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//Challenge 1 - Convert Celsius to Farenheit
+
+function convertToF(celsius) {
+  let fahrenheit = celsius * 9/5 + 32;
+  return fahrenheit;
+}
+
+console.log('30 Celsius is equal to ' + convertToF(30) + ' Farenheit');
+
+
